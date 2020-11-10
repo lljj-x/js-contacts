2946a14e0296ead8f57fa454291f8e8180ba5327
Liu Jun <870395192@qq.com>
2020-11-10 16:11:28 +0800

    chore: 删除本地环境配置

--- packages/lib/scripts/base.config.js
+++ packages/lib/scripts/base.config.js
@@ -22,7 +22,7 @@ const onwarn = (warning) => {
     // Silence circular dependency warning for moment package
     if (
         warning.code === 'CIRCULAR_DEPENDENCY'
-        && !warning.importer.indexOf(path.normalize('src/JsonSchemaForm/'))
+        && !warning.importer.indexOf(path.normalize('src/'))
     ) {
         return;
     }
